b5c13d20dabc2c8c576d1412e6a51b6c2b3b77b2
vlxcoding <vlxcoding@gmail.com>
2023-11-04 22:29:50 -0500

    Edited JS for the Newsletter Subscription Functionality

--- info2180-project1-starter-master/scripts.js
+++ info2180-project1-starter-master/scripts.js
@@ -1,1 +1,28 @@
+// Javon Laing - Info2180 Project 1 #JS
 /* Add your JavaScript to this file */
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector(".newsletter form");
+  const message = document.querySelector(".newsletter .message");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    const emailInput = document.getElementById("email");
+    const enteredEmail = emailInput.value;
+
+    if (enteredEmail && validateEmail(enteredEmail)) {
+      // Valid email address entered
+      message.textContent = `Thank you! Your email address ${enteredEmail} has been added to our mailing list!`;
+    } else {
+      // No email entered or invalid email
+      message.textContent = "Please enter a valid email address.";
+    }
+  });
+});
+
+// Function to validate email format
+function validateEmail(email) {
+  const re = /\S+@\S+\.\S+/;
+  return re.test(email);
+}
